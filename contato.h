339c06828d633b2ea3528ae036171254ff2f2e22
KaiqueSDL <kaiquesdl124@gmail.com>
2024-04-20 21:38:42 -0300

    upload main e contato.h da função salvar binario

--- contato.h
+++ contato.h
@@ -2,11 +2,11 @@
 #define max_nome 50
 #define max_email 150
 
-typedef struct{
-    char nome[max_nome];
-    char sobrenome[max_nome];
-    char email[max_email];
-    int telefone;
+typedef struct {
+  char nome[max_nome];
+  char sobrenome[max_nome];
+  char email[max_email];
+  int telefone;s
 } Contatos;
 
 int criar_contato(Contatos contato[], int *posicao);
@@ -15,7 +15,7 @@ int listar_contatos(Contatos contato[], int *posicao);
 
 int deletar_contato(Contatos contato[], int *posicao);
 
-int salvar_em_binario();
+int salvar_em_binario(Contatos contato[], int *posicao);
 
 int carregar_de_binario();
 
